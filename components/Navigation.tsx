86c36eb51ec76fa253e9d91765975eada82b23d9
SalemFeyzo <f1989salim@gmail.com>
2024-02-26 15:41:49 +0300

    Using clerk SignedIn and SignedOut components

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,18 +1,20 @@
 import { useTranslations } from "next-intl";
 import NavigationLink from "./NavigationLink";
-import { auth } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Navigation() {
-    const t = useTranslations("default.navbar.links");
-    const { userId } = auth();
+  const t = useTranslations("default.navbar.links");
 
-    return (
-        <nav style={{ display: "flex", gap: 10 }}>
-            <NavigationLink href="/">{t("home")}</NavigationLink>
-            <NavigationLink href="/about">{t("about")}</NavigationLink>
-            {!userId ? (<NavigationLink href="/sign-in">{t("sign-in")}</NavigationLink>)
-                : (<NavigationLink href="/dashboard">{t("dashboard")}</NavigationLink>)
-            }
-        </nav>
-    );
+  return (
+    <nav style={{ display: "flex", gap: 10 }}>
+      <NavigationLink href="/">{t("home")}</NavigationLink>
+      <NavigationLink href="/about">{t("about")}</NavigationLink>
+      <SignedIn>
+        <NavigationLink href="/dashboard">{t("dashboard")}</NavigationLink>
+      </SignedIn>
+      <SignedOut>
+        <NavigationLink href="/sign-in">{t("sign-in")}</NavigationLink>
+      </SignedOut>
+    </nav>
+  );
 }
