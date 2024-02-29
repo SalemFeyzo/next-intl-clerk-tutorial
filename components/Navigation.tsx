f6e746c4c73097a17b1b9b7f4826b90075acd6f0
SalemFeyzo <f1989salim@gmail.com>
2024-02-29 23:23:16 +0300

    Nav Menu

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,21 +1,80 @@
-import { useTranslations } from "next-intl";
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
 import NavigationLink from "./NavigationLink";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 
-export default function Navigation() {
-  const t = useTranslations("default.navbar.links");
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
 
-  return (
-    <nav style={{ display: "flex", gap: 10 }}>
-      <NavigationLink href="/">{t("home")}</NavigationLink>
-      <NavigationLink href="/about">{t("about")}</NavigationLink>
-      <NavigationLink href="/blog">{t("blog")}</NavigationLink>
-      <SignedIn>
-        <NavigationLink href="/dashboard">{t("dashboard")}</NavigationLink>
-      </SignedIn>
-      <SignedOut>
-        <NavigationLink href="/sign-in">{t("sign-in")}</NavigationLink>
-      </SignedOut>
-    </nav>
+export default function Navigation() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  return isDesktop ? (
+    <Menu />
+  ) : (
+    <Drawer>
+      <DrawerTrigger>
+        <MenuIcon />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <Menu />
+        <DrawerFooter>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
+
+const Menu = () => {
+  const t = useTranslations("default.navbar.links");
+  const locale = useLocale();
+  return (
+    <NavigationMenu dir={locale === "ar" ? "rtl" : "ltr"}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationLink href="/">{t("home")}</NavigationLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationLink href="/about">{t("about")}</NavigationLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationLink href="/blog">{t("blog")}</NavigationLink>
+        </NavigationMenuItem>
+        <SignedIn>
+          <NavigationMenuItem>
+            <NavigationLink href="/dashboard">{t("dashboard")}</NavigationLink>
+          </NavigationMenuItem>
+        </SignedIn>
+        <SignedOut>
+          <NavigationMenuItem>
+            <NavigationLink href="/sign-in">{t("sign-in")}</NavigationLink>
+          </NavigationMenuItem>
+        </SignedOut>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
--- components/NavigationLink.tsx
+++ components/NavigationLink.tsx
@@ -3,10 +3,14 @@
 import { useSelectedLayoutSegment } from "next/navigation";
 import { ComponentProps } from "react";
 import { Link, pathnames } from "@/lib/navigation";
+import {
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
 
 export default function NavigationLink<
   Pathname extends keyof typeof pathnames
->({ href, ...rest }: ComponentProps<typeof Link<Pathname>>) {
+>({ href, children, ...rest }: ComponentProps<typeof Link<Pathname>>) {
   const selectedLayoutSegment = useSelectedLayoutSegment();
   const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : "/";
   const isActive = pathname === href;
@@ -15,8 +19,16 @@ export default function NavigationLink<
     <Link
       aria-current={isActive ? "page" : undefined}
       href={href}
-      style={{ textDecoration: isActive ? "underline" : "none" }}
       {...rest}
-    />
+      legacyBehavior
+      passHref
+    >
+      <NavigationMenuLink
+        active={isActive}
+        className={navigationMenuTriggerStyle()}
+      >
+        {children}
+      </NavigationMenuLink>
+    </Link>
   );
 }
