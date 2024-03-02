2b57d5648b991342a93fd4c47829189f7ebfd7d6
SalemFeyzo <f1989salim@gmail.com>
2024-03-02 13:57:22 +0300

    Add translations to theme switcher

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -11,12 +11,15 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function ThemeSwitcher() {
   const { setTheme } = useTheme();
+  const locale = useLocale();
+  const t = useTranslations("default.navbar.actions.themeSwitcher");
 
   return (
-    <DropdownMenu>
+    <DropdownMenu dir={locale === "ar" ? "rtl" : "ltr"}>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
@@ -26,13 +29,13 @@ export default function ThemeSwitcher() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          {t("light")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          {t("dark")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          {t("system")}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
