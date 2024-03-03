e7b075aecea76b64730473c5e067cb2f9f9b8f69
SalemFeyzo <f1989salim@gmail.com>
2024-03-03 14:07:17 +0300

    Contentful image loader

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { ImageLoader, ImageLoaderProps } from "next/image";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const contentfulImageLoader: ImageLoader = (props: ImageLoaderProps) => {
+  return `${props.src}?w=${props.width}&q=${props.quality || 75}`;
+};
