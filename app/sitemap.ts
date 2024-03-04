7a25c16e81c288ab2441ea46d370de980f2582e7
SalemFeyzo <f1989salim@gmail.com>
2024-03-04 17:05:58 +0300

    sitemap posts' urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,21 @@
+import { getPosts } from "@/lib/contentful";
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const siteUrl = process.env.SITE_URL;
+  const { items } = await getPosts();
+  const postsUrls = items.flatMap((item) => {
+    return [
+      {
+        url: `${siteUrl}/${item.fields.slug.en as string}`,
+        lastModified: item.sys.updatedAt,
+      },
+      {
+        url: `${siteUrl}/ar/${item.fields.slug.en as string}`,
+        lastModified: item.sys.updatedAt,
+      },
+    ];
+  });
 
   return [
     {
@@ -11,16 +25,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${siteUrl}/ar`,
     },
     {
-      url: `${siteUrl}/tr`,
-    },
-    {
       url: `${siteUrl}/about`,
     },
     {
       url: `${siteUrl}/ar/${encodeURIComponent("حول")}`,
     },
     {
-      url: `${siteUrl}/ar/${encodeURIComponent("hakkında")}`,
+      url: `${siteUrl}/blog`,
     },
+    {
+      url: `${siteUrl}/ar/blog`,
+    },
+    ...postsUrls,
   ];
 }
